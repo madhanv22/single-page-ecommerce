6efcaa9e08d686367f792ab438ebbf927cd91122
madhanv22 <madhankumarv40@gmail.com>
2024-12-17 00:36:46 +0530

    Configured Vite for base path and React plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/frontend_task_madhankumar/",
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+});
