d073add79c9ff333643ae85e4fc4f497ca72ef77
madhanv22 <madhankumarv40@gmail.com>
2024-12-16 23:49:58 +0530

    Remove gh-pages and fix base URL for Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/frontendtask-madhan/",
+  base: "/frontend_task_madhankumar/",
   plugins: [react()],
 })
