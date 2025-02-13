a7489c10ed570255a4af4f033bf36605b7e1c9a7
madhanv22 <madhankumarv40@gmail.com>
2025-02-13 10:22:23 +0530

    name changed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/frontend_task_madhankumar/",
   plugins: [react()],
+  base: "/single-page-ecommerce/",
 });
